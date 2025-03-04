461adfe5e0c42a9637c5fef147758a829b217609
Everson <esteveseverson@gmail.com>
2025-03-04 10:41:31 -0300

    feat: create topic and messages table

--- base/models.py
+++ base/models.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -13,3 +21,14 @@ class Room(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
