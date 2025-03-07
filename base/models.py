4c779efefef36f1782d97324182301f4871327e2
Everson <esteveseverson@gmail.com>
2025-03-07 06:55:14 -0300

    refactor: encapsule to reusable components

--- base/models.py
+++ base/models.py
@@ -33,5 +33,8 @@ class Message(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+
     def __str__(self):
         return self.body[0:50]
