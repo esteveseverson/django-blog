1cb89462e9a9b44b38221479100253a6e75fc841
Everson <esteveseverson@gmail.com>
2025-03-07 06:05:12 -0300

    feat: room participants

--- base/views.py
+++ base/views.py
@@ -87,12 +87,19 @@ def home(request: HttpRequest):
 def room(request: HttpRequest, pk: str):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created_at')
+    participants = room.participants.all()
 
     if request.method == 'POST':
-        Message.objects.create(user=request.user, room=room, body=request.POST.get('body'))
+        Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
 
     return render(request, 'base/room.html', context)
 
