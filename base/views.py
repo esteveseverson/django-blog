620ecdba5709650b46edfeef87efb9b31bf44a94
Everson <esteveseverson@gmail.com>
2025-03-08 10:50:20 -0300

    refactor: add themes, icons and svgs

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
-from base.templates.base.forms import RoomForm
+from base.forms import RoomForm
 
 from .models import Message, Room, Topic
 
@@ -120,7 +120,11 @@ def room(request: HttpRequest, pk: str):
 
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
+    context = {
+        'room': room,
+        'room_messages': room_messages,
+        'participants': participants,
+    }
 
     return render(request, 'base/room.html', context)
 
@@ -128,14 +132,21 @@ def room(request: HttpRequest, pk: str):
 @login_required(login_url='/login')
 def create_room(request: HttpRequest):
     form = RoomForm()
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic').lower()
+        topic, _ = Topic.objects.get_or_create(name=topic_name)
 
-    context = {'form': form}
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics}
 
     return render(request, 'base/room_form.html', context)
 
@@ -144,17 +155,25 @@ def create_room(request: HttpRequest):
 def update_room(request: HttpRequest, pk: str):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('Only the host can update the room')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, _ = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'topics': topics,
+        'room': room,
+    }
 
     return render(request, 'base/room_form.html', context)
 
